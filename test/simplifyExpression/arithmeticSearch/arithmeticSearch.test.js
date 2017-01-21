676fd3fd64714bac586489afcbbbd881e489acc1
Evan Marsland <emars@Evans-MacBook-Pro.local>
2017-01-21 08:16:55 -0500

    fixed rounding issue for negative numbers #72

--- test/simplifyExpression/arithmeticSearch/arithmeticSearch.test.js
+++ test/simplifyExpression/arithmeticSearch/arithmeticSearch.test.js
@@ -20,6 +20,7 @@ describe('evaluate arithmeticSearch', function () {
     ['2+2', '4'],
     ['2*3*5', '30'],
     ['9/4', '9/4'], //  does not divide
+    ['16+ -1943125', '-1943109'] // test toPrecision
   ];
   tests.forEach(t => testArithmeticSearch(t[0], t[1]));
 });
